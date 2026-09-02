c38572c169445ed2506c0a56a48a900e39208a55
Renan C. Vitor <98927761+renancvitor@users.noreply.github.com>
2026-09-02 08:17:07 -0300

    feat(material): add Angular Material foundation (#10)

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -14,10 +14,13 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render material test button', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
+
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ironcore-frontend');
+    const button = compiled.querySelector('button');
+
+    expect(button?.textContent).toContain('IronCore Material Test');
   });
 });
