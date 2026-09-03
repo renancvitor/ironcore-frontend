0aac20bd6c7b806d1d5f30539559204791b277d3
Renan C. Vitor <98927761+renancvitor@users.noreply.github.com>
2026-09-02 21:32:24 -0300

    feat(http): configure HTTP infrastructure and environments (#12)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,8 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideCoreHttp } from './core/http/http.providers';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideCoreHttp()],
 };
